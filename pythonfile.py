097fec953e3fc323fe9f21d6548673738b9f7f8f
Samael3003 <youremail@example.com>
2023-08-29 14:10:14 +0530

    first commit

--- pythonfile.py
+++ pythonfile.py
@@ -8,6 +8,9 @@ repo_directory = '/home/samael-theflamingsword003/GIT-Everyday/GIT-everyday/GIT-
 # Specify the path to the directory containing the files you want to choose from
 source_directory = '/home/samael-theflamingsword003/LEETCODE/CODES'
 
+# Your GitHub repository URL (replace with your own repository URL)
+github_repo_url = 'https://github.com/Samael3003/GIT-everyday.git'
+
 # Change to the Git repository directory
 os.chdir(repo_directory)
 
@@ -24,11 +27,18 @@ with open(os.path.join(source_directory, random_file), 'rb') as file:
 # Initialize the Git repository (if not already)
 subprocess.run(['git', 'init'])
 
-# Loop through each line and commit it
+# Configure Git to use your GitHub credentials (replace with your GitHub username and PAT)
+github_username = 'Samael3003'
+github_pat = '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'
+
+subprocess.run(['git', 'config', '--global', 'user.name', github_username])
+subprocess.run(['git', 'config', '--global', 'user.email', 'youremail@example.com'])
+
+# Loop through each line and commit it to GitHub
 for line in lines:
     # Decode bytes to string using UTF-8, ignoring errors
-    line_text = line.decode('utf-8', errors='ignore')
-    
+    line_text = line.decode('utf-8', errors='ignore').strip()
+
     # Add the line to a new file (e.g., "temp.txt")
     with open('temp.txt', 'w', encoding='utf-8') as temp_file:
         temp_file.write(line_text)
@@ -37,7 +47,11 @@ for line in lines:
     subprocess.run(['git', 'add', 'temp.txt'])
 
     # Commit the change with the line as the commit message
-    subprocess.run(['git', 'commit', '-m', f'Add line: {line_text.strip()}'])
+    subprocess.run(['git', 'commit', '-m', f'Add line: {line_text}'])
+
+    # Push the commit to GitHub
+    subprocess.run(['git', 'remote', 'add', 'origin', github_repo_url])
+    subprocess.run(['git', 'push', '-u', 'origin', 'master'])
 
 # Clean up the temporary file
 os.remove('temp.txt')
